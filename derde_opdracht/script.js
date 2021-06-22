42663be8a14bd2a9907b2556228fef76e6d56328
reneegelderloos <84631679+reneegelderloos@users.noreply.github.com>
2021-06-22 22:01:12 +0200

    Add files via upload

---
+++ derde_opdracht/script.js
@@ -0,0 +1,26 @@
+const age = 5;
+
+if (age >= 18) {
+    console.log("Je mag naar binnen!");
+
+} else {
+    console.log("Je bent te jong, we vragen je vriendelijk buiten te blijven.");
+}
+
+const isFemale = false;
+
+if (isFemale) {
+    console.log("Je mag naar binnen!");
+} else {
+    console.log("Je mag niet naar binnen");
+}
+
+
+
+const driverStatus = "Bob";
+
+if (driverStatus = "Bob") {
+    console.log("Je mag rijden");
+} else {
+    console.log("Je mag niet rijden");
+}
